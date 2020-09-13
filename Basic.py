c7f67a3e674d29db5e8c817383082ae8955ce602
JingYi32 <71211315+JingYi32@users.noreply.github.com>
2020-09-13 17:34:00 +0800

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,9 @@
+#print("Hello")
+'''
+print("This is Python Class")
+print("Welcome to APU.\n\n")
+'''
+print ("Hello.\nThis is Python Class.\nWelcome to APU.")
+
+#"#"is for hide single line of code.
+#''' is for a few line of code.
